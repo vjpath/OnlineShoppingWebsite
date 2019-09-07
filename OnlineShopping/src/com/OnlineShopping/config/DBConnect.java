3392cbf5fb6fe248505dcde744ff30ba9d95b5a8
Vijay Pathawar <vijaypathawar@gmail.com>
2019-09-07 21:43:02 +0530

    Update DBConnect.java

--- OnlineShopping/src/com/OnlineShopping/config/DBConnect.java
+++ OnlineShopping/src/com/OnlineShopping/config/DBConnect.java
@@ -8,8 +8,11 @@ public class DBConnect {
 	public static Connection getConnect() {
 		Connection con=null;
 		try {
+	        //For H2-Database
 		//Class.forName("org.h2.Driver");
 		//con=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/shopping-site","sa","sa");
+			
+			//For MySQLServer 5.0
 			Class.forName("com.mysql.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/vijay","root","root");
 		
